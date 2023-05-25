37b21a104b8a79248bd611d6b2f5779e14e3c613
Bruno Rodrigues <bmonteirorodrigues@gmail.com>
2023-05-25 15:03:56 -0300

    (feat) Adding new games to project

--- python/divination.py
+++ python/divination.py
@@ -1,29 +1,51 @@
-print("*******************")
-print("Divination Game")
-print("*******************")
+import random
 
-secret_number                   = 42
-round                           = 1
-total_desired_rounds_str        = input("Type how many tries you want:")
-total_desired_rounds            = int(total_desired_rounds_str)
+def play():
+    print("*******************")
+    print("Divination Game")
+    print("*******************")
 
-while(round <= total_desired_rounds):
-    
-    print("Round number {} of {}".format(round, total_desired_rounds))
-    response_str                     = input("Type your secret number:")
-    response                         = int(response_str)
-    number_highest_then_response     = response > secret_number
-    number_lowest_then_response      = response < secret_number
-    hit                              = secret_number == response
+    secret_number                   = random.randrange(1,101)
+    round                           = 1
+    difficult_level_str             = input("Choose your difficult level: 1 for Easy, 2 for Normal and 3 for Hard:")
+    difficult_level                 = int(difficult_level_str)
+    user_score                      = 1000
 
-    if(hit):
-        print("You have discovered the secret number!")
+    if(difficult_level == 1):
+        total_rounds = 100
+    elif(difficult_level == 2):
+        total_rounds = 50
     else:
-        if(number_highest_then_response):
-            print("Your number is highest than the secret number!")
-        elif(number_lowest_then_response):
-            print("Your number is lowest than the secret number!")
+        total_rounds = 25    
 
-    round = round + 1
+    print(secret_number)
 
-print("End Game!")
+    for round in range(total_rounds):
+        
+        print("Round number {} of {}".format(round + 1, total_rounds))
+        response_str                     = input("Type your secret number between 1 and 100:")
+        response                         = int(response_str)
+        response_in_correct_range        = response > 1 and response < 100
+        number_highest_then_response     = response > secret_number
+        number_lowest_then_response      = response < secret_number
+        hit                              = secret_number == response
+
+        if(response_in_correct_range == 0):
+            print("You should type a number between 1 and 100!")
+            continue
+
+        if(hit):
+            print("You have discovered the secret number and your socre was {}!".format(user_score))
+            break
+        else:
+            if(number_highest_then_response):
+                print("Your number is highest than the secret number!")
+            elif(number_lowest_then_response):
+                print("Your number is lowest than the secret number!")
+        
+        user_score = user_score - abs(secret_number - response) 
+
+    print("End Game!")
+
+if(__name__ == "__main__"):
+    play()
